e91ae52e5a3e3143080a98de99107a86aa26d9bd
Ashley Jelks <arjelks@gmail.com>
2016-03-31 15:55:15 -0700

    Added a RaisedButton object and implemented MaterialUI customization

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+const style = {
+  margin: 12,
+};
 
 const App = React.createClass({
   render: function(){
     return (
       <div>
-        <p>Hello Universe!!!!</p>
+        <p>Hello Multiverse!!!!</p>
+        <RaisedButton label="Our Button" style={style} primary={true} />  
       </div>
     )
   }
