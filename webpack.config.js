e54995555d1905bbd190541fa1b27dc6f895cbdc
Emily Chen <emily@truss.works>
2016-03-31 21:59:25 -0700

    Remaining changes that I missed in commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/app/index.html', 
+  template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
   devtool: 'eval',
-  entry: './app/app.js',    
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './app/app',
+  ],
   output: {
     filename: 'index_bundle.js',
     path: __dirname + 'dist',
     publicPath: '/dist/'
   },
-  plugins: [HTMLWebpackPluginConfig],
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ['babel', 'eslint-loader']
+        loaders: ['react-hot', 'babel', 'eslint-loader'],
+        include: path.join(__dirname, 'app')
       }
     ]
   }
