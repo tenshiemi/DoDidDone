88056e81e2126eff526cb0c78e006539c76ac070
Ashley Jelks <ashley@truss.works>
2016-04-04 15:02:47 -0700

    Removed comments

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import AppBar from 'material-ui/lib/app-bar';
-
-// class Nav extends React.Component{
-//   render() {
-//     return (
-//       <div>
-//         <AppBar title="Title" />
-//       </div>
-//     );
-//   }
-// };
-
-// export default Nav;
-
-
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 
