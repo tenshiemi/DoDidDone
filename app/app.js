a49486ef22673d59a5eba3d1761fac97d33604a1
Ashley Jelks <ashley@truss.works>
2016-04-06 13:23:07 -0700

    WIP added to do list component

--- app/app.js
+++ app/app.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RaisedButton from 'material-ui/lib/raised-button';
-import Greeting from './components/Greeting';
 import Nav from './components/Nav';
 require('../styles/custom.scss');
 
+const toDoList = ['Some text.'];
+
+let input;
 
 const style = {
   margin: 12
 };
 
+const addToDo = () => {
+  toDoList.push(input.value);
+};
+
 const App = React.createClass({
+  getInitialState: function() {
+    return {
+      toDoItems: toDoList
+    }
+  },
   render: function() {
+    let listItems = this.state.toDoItems.map((toDoItem, index) => {
+      console.log(toDoItem);
+      return <li key={index}> {toDoItem} </li>;
+    });
+
     return (
       <div>
         <Nav />
-        <p>Hello Multiverse!!</p>
-        <RaisedButton label="Our Button" style={style} primary={true} />
-        <Greeting />
+        <ul className="ToDoList">{listItems}</ul>
+        <input ref={node => {
+          input = node;
+        }} />
+        <RaisedButton label="Our Button" style={style} primary={true} onClick={addToDo} />
       </div>
     );
   }
