5596606fdb9e47e747054785d6ebccdd82ef09b4
Emily Chen <emily@truss.works>
2016-04-18 16:02:22 -0700

    Renamed page

--- app/components/Greeting.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Greeting = () => (
-  <h1>Greeting</h1>
-);
-
-export default Greeting;
--- app/router.js
+++ app/router.js
@@ -3,13 +3,13 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import App from './containers/App';
 import TodoListContainer from './containers/TodoListContainer';
-import Greeting from './components/Greeting';
+import About from './containers/About';
 
 export default (
   <Router history={browserHistory}>
     <Route component={ App }>
       <Route path="/" component={ TodoListContainer } />
-        <Route path="about" component={ Greeting } />
+        <Route path="about" component={ About } />
     </Route>
   </Router>
 );
