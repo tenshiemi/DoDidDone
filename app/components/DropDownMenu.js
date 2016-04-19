d0c30cd9fc93f68427fba77420cc348ad5a2c28c
Ashley Jelks <ashley@truss.works>
2016-04-19 16:32:50 -0700

    Added Help component that is linked to Help drop down menu item

--- app/components/DropDownMenu.js
+++ app/components/DropDownMenu.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DropDownMenu from 'material-ui/lib/DropDownMenu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Link } from 'react-router';
 
 injectTapEventPlugin();
 
@@ -10,6 +11,7 @@ const DoDoneDidDropDownMenu = () => (
     <DropDownMenu>
       <MenuItem value={1} primaryText="User Profile" />
       <MenuItem value={2} primaryText="Logout" />
+      <Link to={ 'help' }><MenuItem value={3} primaryText="Help" /></Link>
     </DropDownMenu>
   </div>
 );
--- app/router.js
+++ app/router.js
@@ -4,12 +4,14 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from './containers/App';
 import TodoListContainer from './containers/TodoListContainer';
 import About from './containers/About';
+import Help from './containers/Help';
 
 export default (
   <Router history={ browserHistory }>
     <Route component={ App }>
       <Route path="/" component={ TodoListContainer } />
       <Route path="about" component={ About } />
+      <Route path="help" component={ Help } />
     </Route>
   </Router>
 );
