66d9340aefa0bbad9e175b23da6cfd46907e11d0
Ashley Jelks <ashley@truss.works>
2016-04-18 17:43:41 -0700

    Converted Nav component from functional to class based

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,14 +2,21 @@ import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
 import DoDoneDidDropDownMenu from './DropDownMenu';
 
-const Nav = () => (
-  <div>
-    <AppBar
-      title="DoDoneDid"
-      iconClassNameRight="muidocs-icon-navigation-expand-more"
-    />
-    <DoDoneDidDropDownMenu />
-  </div>
-);
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="DoDoneDid"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <DoDoneDidDropDownMenu />
+      </div>
+    );
+  }
+}
 export default Nav;
---
+++ app/containers/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import DoDoneDidDropDownMenu from './DropDownMenu';
+
+const Nav = () => (
+  <div>
+    <AppBar
+      title="DoDoneDid"
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+    <DoDoneDidDropDownMenu />
+  </div>
+);
+
+export default Nav;
