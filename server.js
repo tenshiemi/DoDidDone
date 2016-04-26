061f2ba36c9ec71df3985384d077354809121b56
Emily Chen <emily@truss.works>
2016-04-26 16:50:21 -0700

    Updated server.js to us es6

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const isDevelopment = (process.env.NODE_ENV !== 'production');
 const static_path = path.join(__dirname, 'dist');
 
 server.use(express.static(static_path))
-  .get('*', function(req, res) {
+  .get('*', (req, res) => {
     res.sendFile('index.html', {
       root: static_path
     });
-  }).listen(process.env.PORT || 8080, function (err) {
+  }).listen(process.env.PORT || 8080, (err) => {
     if (err) { console.log(err) };
     console.log('Listening at localhost:8080');
   });
@@ -26,7 +26,7 @@ if (isDevelopment) {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true,
-  }).listen(3000, 'localhost', function(err, result) {
+  }).listen(3000, 'localhost', (err, result) => {
     if (err) { console.log(err) }
     console.log('Listening at localhost:3000');
   });
