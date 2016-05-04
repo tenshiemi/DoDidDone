c78d50d3a933c1e25afcdaa4482041c57411603e
Ashley Jelks <ashley@truss.works>
2016-05-03 17:15:01 -0700

    Created landing page with AppBar, Login Fields, and Signup Button

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 const style = {
   margin: 12
@@ -7,7 +8,15 @@ const style = {
 
 const Login = () => (
   <div>
-    <RaisedButton label="Login" primary={true} style={style}/>
+    <TextField
+      hintText="Username"
+      aria-label="Username"
+    />
+     <TextField
+      hintText="Password"
+      aria-label="Password"
+    />
+    <RaisedButton label="Login" primary={true} style={style} />
   </div>
 );
 
