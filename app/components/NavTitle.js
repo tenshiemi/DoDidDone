7bf97bf4048bdc506c5ce48a2b632efd6c8be508
Ashley Jelks <ashley@truss.works>
2016-05-05 16:56:19 -0700

    Made DoDoneDid title navigable to home screen

--- app/components/NavTitle.js
+++ app/components/NavTitle.js
@@ -5,10 +5,10 @@ const style = {
   color: 'white',
   fontSize: 'xx-large',
   fontWeight: 'bold',
-  width: 1,
-  position: 'fixed',
-  marginTop: 10
-  // padding: 10
+  width: 1100,
+  position: 'relative',
+  marginTop: 10,
+  padding: 0
 };
 
 export const NavTitle = () => (
