fe2919ddf0dddde4efcb8ba37b4d2e744e0c790f
Emily Chen <emily@truss.works>
2016-05-09 14:22:15 -0700

    Moved inline styles to class and fixed classname bug

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -6,36 +6,30 @@ import { toggleTodoStatus } from '../actions/todosAction';
 import Checkbox from 'material-ui/Checkbox';
 
 export const TodoItem = ({ todoItem, index, dispatch }) => {
-  const style = {
-    width: '40px',
-    display: 'inline-block'
-  };
-
   return (
     <li className="todo-list__item">
-      <div style={ style } >
+      <div className="todo-item__checkbox">
         <Checkbox aria-label="Checkbox" checked={ todoItem.completed } onClick={ () => {
           dispatch(toggleTodoStatus(index, todoItem._id));
         }}/>
       </div>
-      <span className="todo-item__text">{ todoItem.text }&nbsp;&nbsp;
-      <a onClick={ () => {
-        dispatch(removeTodoItem(index, todoItem._id));
-      }}>
-        <i
-          className="material-icons material-icons__delete"
-          aria-label="Delete">delete_forever
-        </i>
-      </a>
-      <a onClick={ () => {
-        const editedTodo = prompt('Edit Todo Item Below:');
-        dispatch(editTodoItem(index, todoItem._id, editedTodo));
-      }}>
-        <i
-          className="material-icons material-icons__edit"
-          aria-label="Delete">mode_edit
-        </i>
-      </a>
+      <span className="todo-list__text">
+        { todoItem.text }&nbsp;&nbsp;
+        <a onClick={ () => {
+          dispatch(removeTodoItem(index, todoItem._id));
+        }}>
+          <i className="material-icons material-icons__delete" aria-label="Delete">
+            delete_forever
+          </i>
+        </a>
+        <a onClick={ () => {
+          const editedTodo = prompt('Edit Todo Item Below:');
+          dispatch(editTodoItem(index, todoItem._id, editedTodo));
+        }}>
+          <i className="material-icons material-icons__edit" aria-label="Delete">
+            mode_edit
+          </i>
+        </a>
       </span>
     </li>
   );
