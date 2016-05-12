7e049d42fe7f415be5d9885498f9e8ec69018464
Ashley Jelks <ashley@truss.works>
2016-05-11 17:48:07 -0700

    converting to class component

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -1,45 +1,46 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { removeTodoItem } from '../actions/todosAction';
-import { editTodoItem } from '../actions/todosAction';
+// import { editTodoItem } from '../actions/todosAction';
 import { toggleTodoStatus } from '../actions/todosAction';
 import Checkbox from 'material-ui/Checkbox';
-import TextField from 'material-ui/TextField';
+// import TextField from 'material-ui/TextField';
 
-export class TodoItem  extends React.Component  {
+export class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
     this.state = {
-      editing: false,
-      todoItem: completed ,
-      index: true,
-      dispatch: true
+      editing: false
     };
-
-    this.setState
   }
   render() {
     return (
       <li className="todo-list__item">
-       <TextField ref="editTodo"/>
         <div className="todo-item__checkbox">
-          <Checkbox aria-label="Checkbox" checked={ todoItem.completed } onClick={ () => {
-            dispatch(toggleTodoStatus(index, todoItem._id));
-          }}/>
+          <Checkbox
+            aria-label="Checkbox"
+            checked={ this.props.todoItem.completed }
+            onClick={ () => {
+              this.props.dispatch(
+                toggleTodoStatus(this.props.index, this.props.todoItem._id));
+            }}/>
         </div>
         <span className="todo-list__text">
           <span id="todo">
-            { todoItem.text }  &nbsp;&nbsp;
+            { this.props.todoItem.text }  &nbsp;&nbsp;
           </span>
           <a onClick={ () => {
-            dispatch(removeTodoItem(index, todoItem._id));
+            this.props.dispatch(removeTodoItem(this.props.index, this.props.todoItem._id));
           }}>
             <i className="material-icons material-icons__delete" aria-label="Delete">
               delete_forever
             </i>
           </a>
           <a onClick={ () => {
-            this.refs.editTodo.focus()
-            const editedTodo = prompt('Edit Todo Item Below:');
-            dispatch(editTodoItem(index, todoItem._id, editedTodo));
+            // const editedTodo = prompt('Edit Todo Item Below:');
+            // this.props.dispatch(editTodoItem(this.props.index,
+            // this.props.todoItem._id, this.props.editedTodo));
           }}>
             <i className="material-icons material-icons__edit" aria-label="Delete">
               mode_edit
@@ -55,14 +56,6 @@ TodoItem.propTypes = {
   todoItem: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
   dispatch: PropTypes.func.isRequired
-};
-const mapStateToProps = (state) => {
-  return {
-    todoItem: state.todos.get('todoItems')
-    editing:
-    index:
-    dispatch:
-  };
 };
 
 export default connect()(TodoItem);
