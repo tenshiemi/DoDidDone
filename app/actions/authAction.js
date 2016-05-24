53f948b83c0f1c0c9ca426c0c067a1f44d09e958
Emily Chen <emily@truss.works>
2016-05-24 16:48:47 -0700

    Saves and removes token from local storage on log in and out

--- app/actions/authAction.js
+++ app/actions/authAction.js
@@ -1,14 +1,21 @@
 import actionHelpers from './actionHelpers';
 
 export const SET_USER_AND_TOKEN = 'SET_USER_AND_TOKEN';
-export const LOG_OUT_USER = 'LOG_OUT_USER';
+export const CLEAR_USER_INFO = 'CLEAR_USER_INFO';
 
 export function setUserAndToken(userInfo) {
   return { type: SET_USER_AND_TOKEN, user: userInfo.email, token: userInfo.token };
 }
 
+export function clearUserInfo() {
+  return { type: CLEAR_USER_INFO };
+}
+
 export function logOutUser() {
-  return { type: LOG_OUT_USER };
+  return (dispatch) => {
+    localStorage.removeItem('id_token');
+    dispatch(clearUserInfo());
+  };
 }
 
 export function signUpUser(newUser) {
@@ -40,7 +47,10 @@ export function logInUser(userCredentials) {
     }).then(actionHelpers.checkStatus)
     .then(actionHelpers.parseJSON)
     .then(actionHelpers.checkSuccess)
-    .then((userInfo) => dispatch(setUserAndToken(userInfo)))
+    .then((userInfo) => {
+      localStorage.setItem('id_token', userInfo.token);
+      dispatch(setUserAndToken(userInfo));
+    })
     .catch(actionHelpers.logError);
   };
 }
