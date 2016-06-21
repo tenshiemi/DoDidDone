01b0420efc18d6dc6887a9a9d65315afe9edbb2e
Ashley Jelks <ashley@truss.works>
2016-06-20 18:12:46 -0700

    cleaned up new lines and indentation

--- app/components/AuthModals.js
+++ app/components/AuthModals.js
@@ -29,41 +29,39 @@ export const AuthModals = ({ state, actions }) => {
   return (
     <div>
       {!localStorage.getItem('id_token') ? (
-            <div>
-              <FlatButton
-                label="Log in"
-                secondary
-                onTouchTap={
-                  () => actions.handleOpen('login')
-                }
-              />
-              <FlatButton
-                label="Sign up"
-                secondary
-                onTouchTap={
-                  () => actions.handleOpen('signup')
-                }
-              />
-            </div>
-          ) : (null)}
-          <Dialog
-            title={modalTitle[state.modalState]}
-            actions={modalActions}
-            modal
-            open={state.open}
-            onRequestClose={actions.handleClose}
-          >
-            {state.modalState === 'login' ? (<LoginForm />) : (<SignupForm />)}
-          </Dialog>
+        <div>
+          <FlatButton
+            label="Log in"
+            secondary
+            onTouchTap={
+              () => actions.handleOpen('login')
+            }
+          />
+          <FlatButton
+            label="Sign up"
+            secondary
+            onTouchTap={
+              () => actions.handleOpen('signup')
+            }
+          />
+        </div>
+      ) : (null)}
+      <Dialog
+        title={modalTitle[state.modalState]}
+        actions={modalActions}
+        modal
+        open={state.open}
+        onRequestClose={actions.handleClose}
+      >
+        {state.modalState === 'login' ? (<LoginForm />) : (<SignupForm />)}
+      </Dialog>
     </div>
   );
 };
-
 
 AuthModals.propTypes = {
   state: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired
 };
-
 
 export default AuthModals;
